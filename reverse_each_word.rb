81a5af61eb73b582644e8be703cbe37c98613e05
Tanner Patterson <tpatterson5492@gmail.com>
2019-10-04 22:22:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require "pry"
 
 def reverse_each_word(sentence)
 split_sentence = sentence.split
-split_sentence.map {|word| 
+split_sentence.collect {|word| 
   word.reverse
 }.join(" ")
 # s.each {|r| r.reverse}
