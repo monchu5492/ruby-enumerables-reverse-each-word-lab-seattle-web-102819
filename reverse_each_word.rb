d2b8cd761030bfc7763760c1bf47d76041bf300c
Tanner Patterson <tpatterson5492@gmail.com>
2019-10-04 22:12:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 require "pry"
 
 
-def reverse_each_word(sentence1)
-binding.pry
-s =[]
-s.push(sentence1)
+def reverse_each_word(sentence)
+split_sentence = sentence.split
+s.map {|word|
+  
+}
 s.each {|r| r.reverse}
 p s.to_s
 end 
